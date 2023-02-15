9a49d45b53d44fed143971586f33db8c7f7c50f9
anay <njagidenzil@gmail.com>
2023-02-15 18:54:25 +0300

    sync last

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,25 @@ const App = () => (
   <div className="bg-primary w-full overflow-hidden">
   <div className={'${styles.paddingX} ${styles.flexCenter}'}>
     <div className={'${styles.boxwidth}'}>
-      Navbar
+      <Navbar />
     </div>
   </div>
 
   <div className={'bg-primary ${styles.flexStart}'}>
     <div className={'${styles.boxwidth}'}>
-      Hero
+      <Hero />
     </div>
   </div>
 
   <div className={'bg-primary ${styles.paddingX} ${styles.flexStart}'}>
     <div className={'${styles.boxwidth}'}>
-      Business
-      Billing
-      CardDeal
-      Testimonials
-      Clients
-      CTA
-      Footer
+      <Business />
+      <Billing />
+      <CardDeal />
+      <Testimonials />
+      <Clients />
+      <CTA />
+      <Footer />
     </div>
   </div>
 
